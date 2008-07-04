cf6d64347857694362de09644b76f293ccaa269f
Dan Smith <dsmith@danplanet.com>
2008-07-04 06:45:18 -0700

    Add general definitions of Memory, Bank, Radio

--- repidr/repidr_common.py
+++ repidr/repidr_common.py
@@ -1,2 +1,44 @@
 class IcomFrame:
     pass
+
+class Memory:
+    freq = 0.0
+    number = 0
+    name = ""
+    vfo = 0
+
+    def __str__(self):
+        return "Memory %i: %.2f (%s) [VFO=%i]" % (self.number,
+                                                  self.freq,
+                                                  self.name,
+                                                  self.vfo)
+
+class Bank:
+    name = "BANK"
+    vfo = 0
+
+class IcomRadio:
+    BAUD_RATE = 9600
+
+    def __init__(self, pipe):
+        self.pipe = pipe
+
+    def get_memory(self, number, vfo=None):
+        pass
+
+    def get_memories(self, vfo=None):
+        pass
+
+    def set_memory(self, memory):
+        pass
+
+    def set_memories(self, memories):
+        pass
+
+    def get_banks(self, vfo=None):
+        pass
+
+    def set_banks(self, vfo=None):
+        pass
+
+    
