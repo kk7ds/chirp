647f7d1f42a93bac018a81b841dd9521fad9a8b7
Dan Smith <dsmith@danplanet.com>
2008-07-16 14:54:18 -0700

    Make clone_to_radio() work for ID800
    Still some cruft that needs cleaning, but it works

--- repidr/id800_ll.py
+++ repidr/id800_ll.py
@@ -94,6 +94,9 @@ def parse_map_for_memory(map):
         _freq = map[addr:addr+3]
         _name = map[addr+11:addr+11+8]
 
+        if len(_freq) != 3:
+            raise Exception("freq != 3 for %i" % i)
+        
         mem = repidr_common.Memory()
         mem.number = i
         mem.name = unpack_name(_name)
@@ -103,20 +106,45 @@ def parse_map_for_memory(map):
 
     return memories
 
-def send_to_id800(pipe, cmd, data, raw=False):
+def send_to_id800(pipe, cmd, data, raw=False, checksum=False):
 
     if raw:
         hed = data
     else:
         hed = ""
         for byte in data:
-            hed += "%02x" % ord(byte)
+            hed += "%02X" % ord(byte)
 
-    frame = "\xfe\xfe\xee\xef%s%s\xfd" % (chr(cmd), hed)
+    if checksum:
+        cs = 0
+        for i in data:
+            cs += ord(i)
 
-    print "Sending: %s" % ["%02x" % ord(x) for x in frame]
+        cs = ((cs ^ 0xFFFF) + 1) & 0xFF
+        cs = "%02X" % cs
+    else:
+        cs =""
 
+    frame = "\xfe\xfe\xee\xef%s%s%s\xfd" % (chr(cmd), hed, cs)
+
+    print "Sending:\n%s" % util.hexprint(frame)
+    
     pipe.write(frame)
+
+    return frame
+
+def get_response(pipe, length=None):
+    def exit_criteria(buf, length):
+        if length is None:
+            return buf.endswith("\xfd")
+        else:
+            return len(buf) == length
+
+    resp = ""
+    while not exit_criteria(resp, length):
+        resp += pipe.read(1)
+
+    return resp
 
 def get_model_data(pipe):
     send_to_id800(pipe, 0xe0, "\x27\x88\x00\x00", raw=True)
@@ -143,9 +171,105 @@ def clone_from_radio(pipe, model_data):
 
     return data
 
+def send_mem_chunk(pipe, mem, start, stop, bs=32):
+    for i in range(start, stop, bs):
+        if i + bs < stop:
+            size = bs
+        else:
+            size = stop - i
+
+        chunk = struct.pack(">HB", i, size) + mem[i:i+size]
+
+        frame = send_to_id800(pipe, 0xe4, chunk, checksum=True)
+        resp = get_response(pipe)
+
+        if frame != resp:
+            print "ERROR: Frame not returned"
+            return False
+
+    return True
+
+def clone_to_radio(pipe, model_data, mem):
+    send_to_id800(pipe, 0xe0, model_data, raw=True)
+    r = get_response(pipe)
+    send_to_id800(pipe, 0xe0, model_data, raw=True)
+    r = get_response(pipe)
+
+    send_to_id800(pipe, 0xe3, model_data, raw=True)
+
+    print "Response to start clone:\n%s" % util.hexprint(get_response(pipe))
+
+    ranges = [(0x0020, 0x2B18, 32),
+              (0x2B18, 0x2B20,  8),
+              (0x2B20, 0x2BE0, 32),
+              (0x2BE0, 0x2BF4, 20),
+              (0x2BF4, 0x2C00, 12),
+              (0x2C00, 0x2DE0, 32),
+              (0x2DE0, 0x2DF4, 20),
+              (0x2DF4, 0x2E00, 12),
+              (0x2E00, 0x2E20, 32),
+
+              (0x2F00, 0x3070, 32),
+
+              (0x30D0, 0x30E0, 16),
+              (0x30E0, 0x3160, 32),
+              (0x3160, 0x3180, 16),
+              (0x3180, 0x31A0, 32),
+              (0x31A0, 0x31B0, 16),
+
+              (0x3220, 0x3240, 32),
+              (0x3240, 0x3260, 16),
+              (0x3260, 0x3560, 32),
+              (0x3560, 0x3580, 16),
+              (0x3580, 0x3720, 32),
+              (0x3720, 0x3780,  8),
+
+              (0x3798, 0x37A0,  8),
+              (0x37A0, 0x37B0, 16),
+              (0x37B0, 0x37B1,  1),
+
+              (0x37D8, 0x37E0,  8),
+              (0x37E0, 0x3898, 32),
+              (0x3898, 0x389A,  2),
+
+              (0x38A8, 0x38C0, 16),]
+
+    for start, stop, bs in ranges:
+        if not send_mem_chunk(pipe, mem, start, stop, bs):
+            break
+
+    send_to_id800(pipe, 0xe5, "Icom Inc\x2eCB", raw=True)
+    resp = get_response(pipe)
+
+    resp = get_response(pipe)
+    # FIXME: Check and report status
+
+def process_payload_frame(mem, data, last_eaddr=0):
+    saddr = int(data[0:4], 16)
+    bytes = int(data[4:6], 16)
+    eaddr = saddr + bytes
+    fdata = data[6:]
+
+    if saddr != last_eaddr:
+        count = saddr - eaddr
+        mem += ("\x00" * count)
+
+    i = 0
+    while i < range(len(fdata)):
+        try:
+            val = int("%s%s" % (fdata[i], fdata[i+1]), 16)
+            i += 2
+            memdata = struct.pack("B", val)
+            mem += memdata
+        except:
+            break
+
+    return mem, eaddr
+
 def process_radio_stream(streamdata):
     mem = ""
-    
+    last_eaddr = 0
+
     while True:
         try:
             start = streamdata.index("\xfe\xfe")
@@ -166,22 +290,8 @@ def process_radio_stream(streamdata):
         data = frame[5:-3]
 
         if cmd == 0xE4:
-            saddr = int(data[0:4], 16)
-            bytes = int(data[4:6], 16)
-            eaddr = saddr + bytes
-            fdata = data[6:]
-        else:
-            continue
+            mem, last_eaddr = process_payload_frame(mem, data, last_eaddr)
 
-        i = 0
-        while i < range(len(fdata)):
-            try:
-                val = int("%s%s" % (fdata[i], fdata[i+1]), 16)
-                i += 2
-                memdata = struct.pack("B", val)
-                mem += memdata
-            except:
-                break
 
     return mem
 
@@ -230,11 +340,22 @@ def test_basic():
 if __name__ == "__main__":
 
     import sys
+    import serial
+    import os
 
     test_basic()
 
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         mm = file(sys.argv[1], "rb")
         map = mm.read()
 
-        parse_map_for_memory(map)
+        #parse_map_for_memory(map)
+
+        pipe = serial.Serial(port=sys.argv[2],
+                             baudrate=9600)
+
+        model = "\x27\x88\x00\x00"
+
+        outlog = file("outlog", "wb", 0)
+
+        clone_to_radio(pipe, model, map)
