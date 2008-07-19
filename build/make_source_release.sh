5399b0e069682ff9f688202483d4426bec0dcd24
Dan Smith <dsmith@danplanet.com>
2008-07-19 08:20:31 -0700

    Fix up source build

--- build/make_source_release.sh
+++ build/make_source_release.sh
@@ -2,22 +2,22 @@
 
 LOCAL_VERSION=
 eval $(cat mainapp.py | grep ^DRATS_VERSION | sed 's/ //g')
-VERSION=${DRATS_VERSION}${LOCAL_VERSION}
-INCLUDE="*.py forms/*.x[ms]l COPYING"
+#VERSION=${DRATS_VERSION}${LOCAL_VERSION}
+VERSION=0.1.1
+INCLUDE="COPYING"
 TMP=$(mktemp -d)
-EXCLUDE="ddt_mb.py ptyhelper.py"
+EXCLUDE=""
 
-RELDIR=d-rats-${VERSION}
+RELDIR=chirp-${VERSION}
 
 DST="${TMP}/${RELDIR}"
 
 mkdir -p $DST
 
-cp -rav d_rats $DST
-cp -av *.py ${DST}/d_rats
-mv ${DST}/d_rats/setup.py ${DST}
+cp -rav --parents chirp/*.py csvdump/*.py $DST
+cp -av *.py ${DST}
 
-cp -rav --parents forms/*.x[ms]l COPYING d-rats ${DST}
+cp -rav $INCLUDE ${DST}
 
 (cd $TMP && tar czf - $RELDIR) > ${RELDIR}.tar.gz
 
