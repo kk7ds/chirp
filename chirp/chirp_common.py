614f7e6c787ee3e3fbcf919a0618d49b4dd42bde
Dan Smith <dsmith@danplanet.com>
2008-07-20 06:51:26 -0700

    Add list of tones and tone freq to chirp.Memory

--- chirp/chirp_common.py
+++ chirp/chirp_common.py
@@ -1,3 +1,13 @@
+TONES = [ 67.0, 69.3, 71.9, 74.4, 77.0, 79.7, 82.5,
+          85.4, 88.5, 91.5, 94.8, 97.4, 100.0, 103.5,
+          107.2, 110.9, 114.8, 118.8, 123.0, 127.3,
+          131.8, 136.5, 141.3, 146.2, 151.4, 156.7,
+          159.8, 162.2, 165.5, 167.9, 171.3, 173.8,
+          177.3, 179.9, 183.5, 186.2, 189.9, 192.8,
+          196.6, 199.5, 203.5, 206.5, 210.7, 218.1,
+          225.7, 229.1, 233.6, 241.8, 250.3, 254.1,
+          ]          
+
 class IcomFrame:
     pass
 
@@ -6,12 +16,14 @@ class Memory:
     number = 0
     name = ""
     vfo = 0
+    tone = 88.5
 
     def __str__(self):
-        return "Memory %i: %.2f (%s) [VFO=%i]" % (self.number,
-                                                  self.freq,
-                                                  self.name,
-                                                  self.vfo)
+        return "Memory %i: %.2f (%s) %.1f [VFO=%i]" % (self.number,
+                                                       self.freq,
+                                                       self.name,
+                                                       self.tone,
+                                                       self.vfo)
 
 class Bank:
     name = "BANK"
