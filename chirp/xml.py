a68ff5354984311759ddb0a241016911550c8e51
Dan Smith <dsmith@danplanet.com>
2008-10-31 17:16:47 -0700

    Fix license header on xml.py

--- chirp/xml.py
+++ chirp/xml.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
+#
+# Copyright 2008 Dan Smith <dsmith@danplanet.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import libxml2
@@ -22,23 +37,6 @@ def validate_doc(doc):
     if err:
         print "---DOC---\n%s\n------" % doc.serialize(format=1)
         raise errors.RadioError("Schema error")
-
-#!/usr/bin/python
-#
-# Copyright 2008 Dan Smith <dsmith@danplanet.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class XMLRadio(chirp_common.IcomFileBackedRadio):
     def __init__(self, pipe):
