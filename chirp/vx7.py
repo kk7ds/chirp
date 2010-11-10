def1053f5e85282161c3ddbe9bcb8da5baeacbae
Dan Smith <dsmith@danplanet.com>
2010-11-09 19:18:47 -0800

    Update VX7 driver to use bitwise

--- chirp/vx7.py
+++ chirp/vx7.py
@@ -15,7 +15,70 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from chirp import chirp_common, yaesu_clone, vx7_ll
+from chirp import chirp_common, yaesu_clone
+from chirp import bitwise
+
+mem_format = """
+#seekto 0x0611;
+u8 checksum1;
+
+#seekto 0x0691;
+u8 checksum2;
+
+#seekto 0x3F52;
+u8 checksum3;
+
+#seekto 0x1202;
+struct {
+  u8 even_pskip:1,
+     even_skip:1,
+     even_valid:1,
+     even_masked:1,
+     odd_pskip:1,
+     odd_skip:1,
+     odd_valid:1,
+     odd_masked:1;
+} flags[225];
+
+#seekto 0x1322;
+struct {
+  u8   unknown1;
+  u8   power:2,
+       duplex:2,
+       tune_step:4;
+  bbcd freq[3];
+  u8   zeros1:2,
+       ones:2,
+       zeros2:2,
+       mode:2;
+  u8   name[8];
+  u8 zero;
+  bbcd offset[3];
+  u8   zeros3:3,
+       tone:5;
+  u8   zero:1,
+       dcs:7;
+  u8   zeros:6,
+       tmode:2;
+  u8   charset;
+} memory[450];
+"""
+
+DUPLEX = ["", "-", "+", "split"]
+MODES  = ["FM", "AM", "WFM", "FM"] # last is auto
+TMODES = ["", "Tone", "TSQL", "DTCS"]
+STEPS = list(chirp_common.TUNING_STEPS)
+STEPS.remove(6.25)
+STEPS.remove(30.0)
+STEPS.append(100.0)
+STEPS.append(9.0)
+
+CHARSET = ["%i" % int(x) for x in range(0, 10)] + \
+    [" "] + \
+    [chr(x) for x in range(ord("A"), ord("Z")+1)] + \
+    [chr(x) for x in range(ord("a"), ord("z")+1)] + \
+    list(".,:;!\"#$%&'()*+-.=<>?@[?]^_\\{|}") + \
+    list("?" * 100)
 
 class VX7Radio(yaesu_clone.YaesuCloneModeRadio):
     BAUD_RATE = 19200
@@ -25,6 +88,31 @@ class VX7Radio(yaesu_clone.YaesuCloneModeRadio):
     _memsize = 16211
     _block_lengths = [ 10, 8, 16193 ]
     _block_size = 8
+
+    def _update_checksum(self):
+        cs = 0
+        for i in range(0x0592, 0x0611):
+            cs += ord(self._mmap[i])
+        cs %= 256
+        print "Checksum1 old=%02x new=%02x" % (self._memobj.checksum1, cs)
+        self._memobj.checksum1 = cs
+    
+        cs = 0
+        for i in range(0x0612, 0x0691):
+            cs += ord(self._mmap[i])
+        cs %= 256
+        print "Checksum2 old=%02x new=%02x" % (self._memobj.checksum2, cs)
+        self._memobj.checksum2 = cs
+    
+        cs = 0
+        for i in range(0x0000, 0x3F52):
+            cs += ord(self._mmap[i])
+        cs %= 256
+        print "Checksum3 old=%02x new=%02x" % (self._memobj.checksum3, cs)
+        self._memobj.checksum3 = cs
+
+    def process_mmap(self):
+        self._memobj = bitwise.parse(mem_format, self._mmap)
 
     def get_features(self):
         rf = chirp_common.RadioFeatures()
@@ -38,21 +126,64 @@ class VX7Radio(yaesu_clone.YaesuCloneModeRadio):
     def get_raw_memory(self, number):
         return vx7_ll.get_raw_memory(self._mmap, number)
 
-    def _update_checksum(self):
-        vx7_ll.update_checksum(self._mmap)
-
     def get_memory(self, number):
-        return vx7_ll.get_memory(self._mmap, number)
+        _mem = self._memobj.memory[number-1]
+        _flag = self._memobj.flags[(number-1)/2]
 
-    def set_memory(self, memory):
-        if not self._mmap:
-            self.sync_in()
+        nibble = ((number-1) % 2) and "even" or "odd"
+        used = _flag["%s_masked" % nibble] and _flag["%s_valid" % nibble]
+        pskip = _flag["%s_pskip" % nibble]
+        skip = _flag["%s_skip" % nibble]
 
-        if memory.empty:
-            self._mmap = vx7_ll.erase_memory(self._mmap, memory.number)
-        else:
-            self._mmap = vx7_ll.set_memory(self._mmap, memory)
+        mem = chirp_common.Memory()
+        mem.number = number
+        if not used:
+            mem.empty = True
+            return mem
 
+        mem.freq = int(_mem.freq) / 1000.0
+        mem.offset = int(_mem.offset) / 1000.0
+        mem.rtone = mem.ctone = chirp_common.TONES[_mem.tone]
+        mem.tmode = TMODES[_mem.tmode]
+        mem.duplex = DUPLEX[_mem.duplex]
+        mem.mode = MODES[_mem.mode]
+        mem.dtcs = chirp_common.DTCS_CODES[_mem.dcs]
+        mem.tuning_step = STEPS[_mem.tune_step]
+        mem.skip = pskip and "P" or skip and "S" or ""
+
+        for i in _mem.name:
+            mem.name += CHARSET[i]
+
+        return mem
+
+    def set_memory(self, mem):
+        _mem = self._memobj.memory[mem.number-1]
+        _flag = self._memobj.flags[(mem.number-1)/2]
+
+        nibble = ((mem.number-1) % 2) and "even" or "odd"
+        
+        _flag["%s_masked" % nibble] = not mem.empty
+        _flag["%s_valid" % nibble] = not mem.empty
+        if mem.empty:
+            print "Mem is empty %i %i" % (_flag["%s_masked" % nibble],
+                                          _flag["%s_valid" % nibble])
+            return
+
+        _mem.freq = int(mem.freq * 1000)
+        _mem.offset = int(mem.offset * 1000)
+        _mem.tone = chirp_common.TONES.index(mem.rtone)
+        _mem.tmode = TMODES.index(mem.tmode)
+        _mem.duplex = DUPLEX.index(mem.duplex)
+        _mem.mode = MODES.index(mem.mode)
+        _mem.dcs = chirp_common.DTCS_CODES.index(mem.dtcs)
+        _mem.tune_step = STEPS.index(mem.tuning_step)
+
+        _flag["%s_pskip" % nibble] = mem.skip == "S" and 1 or 0
+        _flag["%s_skip" % nibble] = mem.skip == "P" and 1 or 0
+
+        for i in range(0, 8):
+            _mem.name[i] = CHARSET.index(mem.name.ljust(8)[i])
+        
     def get_banks(self):
         return []
 
