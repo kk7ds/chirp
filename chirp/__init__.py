3065d7640d68f85a4cf12652dc971898fa0799fc
Dan Smith <dsmith@danplanet.com>
2012-01-19 08:48:01 -0800

    Update the version number to: 1. Stay ahead of the current release 2. Indicate that it's a "dev" version (i.e. from the repo)

--- chirp/__init__.py
+++ chirp/__init__.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CHIRP_VERSION="0.1.11"
+CHIRP_VERSION="0.1.13dev"
