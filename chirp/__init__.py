160e47586fde565c9ee6aa2d442a0cc8b9c6ed80
Dan Smith <dsmith@danplanet.com>
2012-03-09 08:07:18 -0800

    0.2.0 Release

--- chirp/__init__.py
+++ chirp/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CHIRP_VERSION="0.1.13dev"
+CHIRP_VERSION="0.2.0"
 
 import os
 import sys
