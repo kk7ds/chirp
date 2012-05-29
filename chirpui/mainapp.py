961d9217877413a193b33fbf6aad359deca1cd00
Dan Smith <dsmith@danplanet.com>
2012-05-29 13:00:30 -0700

    fips: Pennsylvania FIPS code was wrong. #117

--- chirpui/mainapp.py
+++ chirpui/mainapp.py
@@ -84,7 +84,7 @@ FIPS_CODES = {
     "Ohio"                 : 39,
     "Oklahoma"             : 40,
     "Oregon"               : 41,
-    "Pennsylvania"         : 32,
+    "Pennsylvania"         : 42,
     "Puerto Rico"          : 72,
     "Rhode Island"         : 44,
     "South Carolina"       : 45,
