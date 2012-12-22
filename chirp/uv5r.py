6f23aca1ab25381ae8bb38bbe77b6f5b8a2b25b6
Dan Smith <dsmith@danplanet.com>
2012-12-21 17:20:18 -0800

    [uv5r] Allow backlight timeout values up to 24s
    Related to #366

--- chirp/uv5r.py
+++ chirp/uv5r.py
@@ -596,7 +596,7 @@ class BaofengUV5R(chirp_common.CloneModeRadio,
         advanced.append(rs)
 
         rs = RadioSetting("abr", "Backlight Timeout",
-                          RadioSettingValueInteger(0, 10, _settings.abr))
+                          RadioSettingValueInteger(0, 24, _settings.abr))
         basic.append(rs)
 
         rs = RadioSetting("tdr", "Dual Watch",
