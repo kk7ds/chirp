fc5051d177144ade294d30c333334b1b69932f4a
Dan Smith <dsmith@danplanet.com>
2013-02-13 15:53:54 -0800

    Avoid GTK warnings in unit tests
    Introduced while fixing #541

--- tests/unit/base.py
+++ tests/unit/base.py
@@ -1,6 +1,9 @@
 import unittest
 import mox
 
+import warnings
+warnings.simplefilter('ignore', Warning)
+
 class BaseTest(unittest.TestCase):
     def setUp(self):
         __builtins__['_'] = lambda s: s
