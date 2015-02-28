585adb1bbf8ef7d5c033acef0a7603f1027504a5
Dan Smith <dsmith@danplanet.com>
2015-02-27 17:17:27 -0800

    Run pep8 in a virtualenv with a pep8==1.4.6
    This makes our test runner generate a virtualenv, install pep8==1.4.6
    inside, and run our pep8 test using it.
    
    Fallout from issue #2355

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+VENV="${TMPDIR:-/tmp}/venv"
+
 function record_failure() {
     FAILED="$1 $FAILED"
 }
@@ -26,9 +28,29 @@ function style_tests() {
     ./tools/checkpatch.sh
 }
 
+function ensure_test_venv() {
+    virtualenv=$(which virtualenv)
+    if [ ! -x "$virtualenv" ]; then
+	echo 'Please install virtualenv'
+	return 1
+    fi
+    if [ ! -d "$VENV" ]; then
+	virtualenv "$VENV"
+    fi
+    return 0
+}
+
 function pep8() {
+    ensure_test_venv
+    if [ $? -ne 0 ]; then
+	record_failure pep8
+	return
+    fi
+    source ${VENV}/bin/activate
+    pip install pep8==1.4.6 >${VENV}/pep8.log 2>&1
     echo "Checking for PEP8 regressions..."
-    time ./tools/cpep8.py --stats
+    time ./tools/cpep8.py
+    deactivate
 }
 
 if test -z "${TESTS[*]}"; then
