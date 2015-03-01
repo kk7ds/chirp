63154deb517b2dfb6478602defce26b83817c84d
Zach Welch <zach@mandolincreekfarm.com>
2015-02-28 16:27:50 -0800

    Improve cpep8.py (#2355)
    This patch expands cpep8.manifest to contain all files, allowing the
    cpep8.py script to use it instead of scanning for files.  Scanning can
    now be done with --scan.  If both --scan and --update options are given,
    the manifest file will be updated with the scan results.
    
    Altogether, this should fix problems that Windows users were seeing,
    caused by mismatches between the dynamic manifest and the blacklist
    (due to different path separators).

--- tools/cpep8.py
+++ tools/cpep8.py
@@ -23,8 +23,6 @@ import logging
 import argparse
 import pep8
 
-scriptdir = os.path.dirname(sys.argv[0])
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--all", action="store_true",
                     help="Check all files, ignoring blacklist")
@@ -32,8 +30,10 @@ parser.add_argument("-d", "--dir", action="store", default=".",
                     help="Root directory of source tree")
 parser.add_argument("-s", "--stats", action="store_true",
                     help="Only show statistics")
+parser.add_argument("-S", "--scan", action="store_true",
+                    help="Scan for additional files")
 parser.add_argument("-u", "--update", action="store_true",
-                    help="Update the blacklist file")
+                    help="Update manifest/blacklist files")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="Display list of checked files")
 parser.add_argument("files", metavar="file", nargs='*',
@@ -48,16 +48,25 @@ def file_to_lines(name):
     fh.close()
     return lines
 
-# read manifest and search for python files
-manifest = file_to_lines(os.path.join(scriptdir, "cpep8.manifest"))
-for root, dirs, files in os.walk(args.dir):
-    for f in files:
-        if f.endswith('.py'):
-            manifest.append(os.path.join(root, f))
 
-# read the blacklisted source files
+scriptdir = os.path.dirname(sys.argv[0])
+manifest_filename = os.path.join(scriptdir, "cpep8.manifest")
 blacklist_filename = os.path.join(scriptdir, "cpep8.blacklist")
-blacklist = file_to_lines(blacklist_filename)
+
+manifest = []
+if args.scan:
+    for root, dirs, files in os.walk(args.dir):
+        for f in files:
+            filename = os.path.join(root, f)
+            if f.endswith('.py'):
+                manifest.append(filename)
+                continue
+            with file(filename, "r") as fh:
+                shebang = fh.readline()
+                if shebang.startswith("#!/usr/bin/env python"):
+                    manifest.append(filename)
+else:
+    manifest += file_to_lines(manifest_filename)
 
 if args.update:
     bad = []
@@ -66,16 +75,24 @@ if args.update:
         results = checker.check_files([f])
         if results.total_errors:
             bad.append(f)
+
     with file(blacklist_filename, "w") as fh:
         print >>fh, """\
 # cpep8.blacklist: The list of files that do not meet PEP8 standards.
 # DO NOT ADD NEW FILES!!  Instead, fix the code to be compliant.
 # Over time, this list should shrink and (eventually) be eliminated."""
         print >>fh, "\n".join(sorted(bad))
+
+    if args.scan:
+        with file(manifest_filename, "w") as fh:
+            print >>fh, "\n".join(sorted(manifest))
     sys.exit(0)
 
 if args.files:
     manifest = args.files
+
+# read the blacklisted source files
+blacklist = file_to_lines(blacklist_filename)
 
 check_list = []
 for f in manifest:
