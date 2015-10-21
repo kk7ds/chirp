874e36ad62020b0f38849fefd77162d9a7d87f43
Eric Dropps <kc1ckh@kc1ckh.com>
2015-10-20 22:01:41 -0400

    [ID-51][ID-51 Plus] Add support for ID-51 Plus/Anniversary Edition (Fix #1961)
    This patch adds support for the Icom ID-51 Anniversary and Plus Editions
    
    Due to memory size conflicts, this also contains an update to the ID-51 driver
    that enhances detetion logic.  The ID-51 has also been renamed from ID-51A
    as the A and E editions are identical (image-wise)
    
    Confirmed and tested with both a real ID-51A Plus and an ID-51E Plus

--- chirp/drivers/id51plus.py
+++ chirp/drivers/id51plus.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Dan Smith <dsmith@danplanet.com>
+# Copyright 2015 Eric Dropps <kc1ckh@kc1ckh.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,6 +16,8 @@ import logging
 
 from chirp.drivers import id31
 from chirp import directory, bitwise
+
+LOG = logging.getLogger(__name__)
 
 MEM_FORMAT = """
 struct {
@@ -50,7 +52,8 @@ u8 pskp_flags[69];
 
 #seekto 0x6B40;
 struct {
-  u8 bank;
+  u8 unknown:3,
+     bank:5;
   u8 index;
 } banks[500];
 
@@ -59,11 +62,12 @@ struct {
   char name[16];
 } bank_names[26];
 
+
 #seekto 0xA8C0;
 struct {
   u24 freq;
-  u16 offset;
-  u8 unknown1[3];
+  u16 offset; 
+  u8 unknown1[4];
   u8 call[7];
   char name[16];
   char subname[8];
@@ -75,30 +79,37 @@ struct {
   u8 call[7];
 } rptcall[750];
 
-#seekto 0x14E60;
+#seekto 0x14FBE;
+struct {
+ char name[16];
+} rptgroup_names[30];
+
+#seekto 0x1519E;
 struct {
   char call[8];
   char tag[4];
 } mycall[6];
 
-#seekto 0x14EA8;
+#seekto 0x151E6;
 struct {
   char call[8];
 } urcall[200];
 
+#seekto 0x15826;
+struct {
+  char name[16];
+} urcallname[200];
 """
-LOG = logging.getLogger(__name__)
-
 
 @directory.register
-class ID51Radio(id31.ID31Radio):
-    """Icom ID-51"""
-    MODEL = "ID-51"
+class ID51PLUSRadio(id31.ID31Radio):
+    """Icom ID-51 Plus/50th Anniversary"""
+    MODEL = "ID-51 Plus"
 
     _memsize = 0x1FB40
-    _model = "\x33\x90\x00\x01"
+    _model = "\x33\x90\x00\x02"
     _endframe = "Icom Inc\x2E\x44\x41"
-
+    _bank_class = id31.ID31Bank
     _ranges = [(0x00000, 0x1FB40, 32)]
 
     MODES = {0: "FM", 1: "NFM", 3: "AM", 5: "DV"}
@@ -115,20 +126,46 @@ class ID51Radio(id31.ID31Radio):
             LOG.debug('File has correct memory size, '
                       'checking 20 bytes at offset 0x1AF40')
             snip = filedata[0x1AF40:0x1AF60]
-            if snip == ('\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'
+            if snip != ('\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'
                         '\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'
                         '\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'
                         '\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'):
-                LOG.debug('bytes matched ID-51 Signature')
+                LOG.debug('bytes matched ID-51 Plus Signature')
                 return True
             else:
-                LOG.debug('bytes did not match ID-51 Signature')
+                LOG.debug('bytes did not match ID-51 Plus Signature')
         return False
 
+    def _get_bank(self, loc):
+        _bank = self._memobj.banks[loc]
+        LOG.debug("Bank Value for location %s is %s" % (loc, _bank.bank))
+        if _bank.bank == 0x1F:
+            return None         
+        else:
+            return _bank.bank
+
+    def _set_bank(self, loc, bank):
+        _bank = self._memobj.banks[loc]
+        if bank is None:
+            _bank.bank = 0x1F
+        else:
+            _bank.bank = bank
+
     def get_features(self):
-        rf = super(ID51Radio, self).get_features()
-        rf.valid_bands = [(108000000, 174000000), (400000000, 479000000)]
+        rf = super(ID51PLUSRadio, self).get_features()
+        rf.valid_bands = [(108000000, 174000000), (380000000, 479000000)]
         return rf
+
+    def get_repeater_call_list(self):
+        calls = []
+        # Unlike previos DStar radios, there is not a seperate repeater
+        # callsign list. It's only the DV Memory banks.
+        for repeater in self._memobj.repeaters:
+            call = id31._decode_call(repeater.call)
+            if call == "CALLSIGN":
+                call = ""
+            calls.append(call.rstrip())
+        return calls
 
     def process_mmap(self):
         self._memobj = bitwise.parse(MEM_FORMAT, self._mmap)
