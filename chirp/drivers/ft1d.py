79bd0a42da66ad75315d667224f9e17b54a8f810
Wade Simmons <wade@wades.im>
2017-05-24 11:15:55 -0400

    [FTM-3200D] Add support for Yaesu FTM-3200D
    This patch adds basic CHIRP support for programming the FTM-3200D. This radio
    shares most of its memory layout with the FT1D, so it has been created as a
    subclass of that radio.
    
    #4279

--- chirp/drivers/ft1d.py
+++ chirp/drivers/ft1d.py
@@ -27,7 +27,7 @@ from textwrap import dedent
 
 LOG = logging.getLogger(__name__)
 
-MEM_FORMAT = """
+MEM_SETTINGS_FORMAT = """
 #seekto 0x049a;
 struct {
   u8 vfo_a;
@@ -141,17 +141,21 @@ struct {
   u8 unknown[2];
   u8 name[16];
 } bank_info[24];
+"""
 
+MEM_FORMAT = """
 #seekto 0x2D4A;
 struct {
-  u8 unknown1;
+  u8 unknown0:2,
+     mode_alt:1,  // mode for FTM-3200D
+     unknown1:5;
   u8 mode:2,
      duplex:2,
      tune_step:4;
   bbcd freq[3];
   u8 power:2,
-     unknown2:4,
-     tone_mode:2;
+     unknown2:2,
+     tone_mode:4;
   u8 charsetbits[2];
   char label[16];
   bbcd offset[3];
@@ -160,7 +164,7 @@ struct {
   u8 unknown6:1,
      dcs:7;
   u8 unknown7[3];
-} memory[900];
+} memory[%d];
 
 #seekto 0x280A;
 struct {
@@ -170,8 +174,10 @@ struct {
      skip:1,
      used:1,
      valid:1;
-} flag[900];
+} flag[%d];
+"""
 
+MEM_APRS_FORMAT = """
 #seekto 0xbeca;
 struct {
   u8 rx_baud;
@@ -334,7 +340,9 @@ struct {
   char path_and_body[66];
   u8 unknown[70];
 } aprs_message_pkt[60];
+"""
 
+MEM_CHECKSUM_FORMAT = """
 #seekto 0x1FDC9;
 u8 checksum;
 """
@@ -500,11 +508,7 @@ class FT1BankModel(chirp_common.BankModel):
         return banks
 
 
-def _wipe_memory(mem):
-    mem.set_raw("\x00" * (mem.size() / 8))
-    mem.unknown1 = 0x05
-
-
+# Note: other radios like FTM3200Radio subclass this radio
 @directory.register
 class FT1Radio(yaesu_clone.YaesuCloneModeRadio):
     """Yaesu FT1DR"""
@@ -517,7 +521,9 @@ class FT1Radio(yaesu_clone.YaesuCloneModeRadio):
     _memsize = 130507
     _block_lengths = [10, 130497]
     _block_size = 32
-    _mem_params = (0xFECA,         # APRS beacon metadata address.
+    _mem_params = (900,            # size of memories array
+                   900,            # size of flags array
+                   0xFECA,         # APRS beacon metadata address.
                    60,             # Number of beacons stored.
                    0x1064A,        # APRS beacon content address.
                    134,            # Length of beacon data stored.
@@ -610,7 +616,9 @@ class FT1Radio(yaesu_clone.YaesuCloneModeRadio):
         return rp
 
     def process_mmap(self):
-        self._memobj = bitwise.parse(MEM_FORMAT % self._mem_params, self._mmap)
+        mem_format = MEM_SETTINGS_FORMAT + MEM_FORMAT + MEM_APRS_FORMAT + \
+                MEM_CHECKSUM_FORMAT
+        self._memobj = bitwise.parse(mem_format % self._mem_params, self._mmap)
 
     def get_features(self):
         rf = chirp_common.RadioFeatures()
@@ -632,7 +640,8 @@ class FT1Radio(yaesu_clone.YaesuCloneModeRadio):
         return rf
 
     def get_raw_memory(self, number):
-        return repr(self._memobj.memory[number])
+        return "\n".join([repr(self._memobj.memory[number - 1]),
+                          repr(self._memobj.flag[number - 1])])
 
     def _checksums(self):
         return [yaesu_clone.YaesuChecksum(0x064A, 0x06C8),
@@ -666,20 +675,52 @@ class FT1Radio(yaesu_clone.YaesuCloneModeRadio):
         mem.freq = chirp_common.fix_rounded_step(int(_mem.freq) * 1000)
         mem.offset = int(_mem.offset) * 1000
         mem.rtone = mem.ctone = chirp_common.TONES[_mem.tone]
-        mem.tmode = TMODES[_mem.tone_mode]
+        self._get_tmode(mem, _mem)
         mem.duplex = DUPLEX[_mem.duplex]
         if mem.duplex == "split":
             mem.offset = chirp_common.fix_rounded_step(mem.offset)
-        mem.mode = MODES[_mem.mode]
+        mem.mode = self._decode_mode(_mem)
         mem.dtcs = chirp_common.DTCS_CODES[_mem.dcs]
         mem.tuning_step = STEPS[_mem.tune_step]
-        mem.power = POWER_LEVELS[3 - _mem.power]
+        mem.power = self._decode_power_level(_mem)
         mem.skip = flag.pskip and "P" or flag.skip and "S" or ""
 
-        charset = ''.join(CHARSET).ljust(256, '.')
-        mem.name = str(_mem.label).rstrip("\xFF").translate(charset)
+        mem.name = self._decode_label(_mem)
 
         return mem
+
+    def _decode_label(self, mem):
+        charset = ''.join(CHARSET).ljust(256, '.')
+        return str(mem.label).rstrip("\xFF").translate(charset)
+
+    def _encode_label(self, mem):
+        label = "".join([chr(CHARSET.index(x)) for x in mem.name.rstrip()])
+        return self._add_ff_pad(label, 16)
+
+    def _encode_charsetbits(self, mem):
+        # We only speak english here in chirpville
+        return [0x00, 0x00]
+
+    def _decode_power_level(self, mem):
+        return POWER_LEVELS[3 - mem.power]
+
+    def _encode_power_level(self, mem):
+        return 3 - POWER_LEVELS.index(mem.power)
+
+    def _decode_mode(self, mem):
+        return MODES[mem.mode]
+
+    def _encode_mode(self, mem):
+        return MODES.index(mem.mode)
+
+    def _get_tmode(self, mem, _mem):
+        mem.tmode = TMODES[_mem.tone_mode]
+
+    def _set_tmode(self, _mem, mem):
+        _mem.tone_mode = TMODES.index(mem.tmode)
+
+    def _set_mode(self, _mem, mem):
+        _mem.mode = self._encode_mode(mem)
 
     def _debank(self, mem):
         bm = self.get_bank_model()
@@ -693,7 +734,7 @@ class FT1Radio(yaesu_clone.YaesuCloneModeRadio):
         self._debank(mem)
 
         if not mem.empty and not flag.valid:
-            _wipe_memory(_mem)
+            self._wipe_memory(_mem)
 
         if mem.empty and flag.valid and not flag.used:
             flag.valid = False
@@ -714,24 +755,27 @@ class FT1Radio(yaesu_clone.YaesuCloneModeRadio):
         _mem.freq = int(mem.freq / 1000)
         _mem.offset = int(mem.offset / 1000)
         _mem.tone = chirp_common.TONES.index(mem.rtone)
-        _mem.tone_mode = TMODES.index(mem.tmode)
+        self._set_tmode(_mem, mem)
         _mem.duplex = DUPLEX.index(mem.duplex)
-        _mem.mode = MODES.index(mem.mode)
+        self._set_mode(_mem, mem)
         _mem.dcs = chirp_common.DTCS_CODES.index(mem.dtcs)
         _mem.tune_step = STEPS.index(mem.tuning_step)
         if mem.power:
-            _mem.power = 3 - POWER_LEVELS.index(mem.power)
+            _mem.power = self._encode_power_level(mem)
         else:
             _mem.power = 0
 
-        label = "".join([chr(CHARSET.index(x)) for x in mem.name.rstrip()])
-        _mem.label = self._add_ff_pad(label, 16)
-        # We only speak english here in chirpville
-        _mem.charsetbits[0] = 0x00
-        _mem.charsetbits[1] = 0x00
+        _mem.label = self._encode_label(mem)
+        charsetbits = self._encode_charsetbits(mem)
+        _mem.charsetbits[0], _mem.charsetbits[1] = charsetbits
 
         flag.skip = mem.skip == "S"
         flag.pskip = mem.skip == "P"
+
+    @classmethod
+    def _wipe_memory(cls, mem):
+        mem.set_raw("\x00" * (mem.size() / 8))
+        mem.unknown1 = 0x05
 
     def get_bank_model(self):
         return FT1BankModel(self)
