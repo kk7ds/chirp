5cca12ee7ea5154c1a50dd94428410ad1ed9795f
Dan Smith <dsmith@danplanet.com>
2017-10-12 11:35:47 -0700

    Make tests module generate a unittest-compatible interface
    This lets us use the integrated unittest runner, which does things
    like report failures at the end, stdio buffering, etc. You can still
    use the old tests/run_tests if desired (for now), but this makes it
    much nicer for the build system.
    
    Loosely related to #5237

---
+++ tests/__init__.py
@@ -0,0 +1,99 @@
+import glob
+import os
+import shutil
+import sys
+import tempfile
+import unittest
+
+from chirp import directory
+
+import run_tests
+
+
+class TestAdapterMeta(type):
+    def __new__(cls, name, parents, dct):
+        return super(TestAdapterMeta, cls).__new__(cls, name, parents, dct)
+
+
+class TestAdapter(unittest.TestCase):
+    RADIO_CLASS = None
+    SOURCE_IMAGE = None
+
+    def shortDescription(self):
+        test = self.id().split('.')[-1].replace('test_', '').replace('_', ' ')
+        return 'Testing %s %s' % (self.RADIO_CLASS.get_name(), test)
+
+    def setUp(self):
+        self._out = run_tests.TestOutputANSI()
+        self.testimage = tempfile.mktemp('.img')
+        shutil.copy(self.SOURCE_IMAGE, self.testimage)
+
+    def tearDown(self):
+        os.remove(self.testimage)
+
+    def _runtest(self, test):
+        tw = run_tests.TestWrapper(self.RADIO_CLASS,
+                                   self.testimage)
+        testcase = test(tw)
+        testcase.prepare()
+        try:
+            testcase.run()
+        except run_tests.TestSkippedError as e:
+            raise unittest.SkipTest(str(e))
+        finally:
+            testcase.cleanup()
+
+    def test_copy_all(self):
+        self._runtest(run_tests.TestCaseCopyAll)
+
+    def test_brute_force(self):
+        self._runtest(run_tests.TestCaseBruteForce)
+
+    def test_edges(self):
+        self._runtest(run_tests.TestCaseEdges)
+
+    def test_settings(self):
+        self._runtest(run_tests.TestCaseSettings)
+
+    def test_banks(self):
+        self._runtest(run_tests.TestCaseBanks)
+
+    def test_detect(self):
+        self._runtest(run_tests.TestCaseDetect)
+
+    def test_clone(self):
+        self._runtest(run_tests.TestCaseClone)
+
+
+def _get_sub_devices(rclass, testimage):
+    try:
+        tw = run_tests.TestWrapper(rclass, '/etc/localtime')
+    except Exception as e:
+        tw = run_tests.TestWrapper(rclass, testimage)
+
+    rf = tw.do("get_features")
+    if rf.has_sub_devices:
+        return [r.__class__ for r in tw.do("get_sub_devices")]
+    else:
+        return [rclass]
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+
+    images = glob.glob("tests/images/*.img")
+    tests = [os.path.splitext(os.path.basename(img))[0] for img in images]
+
+    for test in tests:
+        image = os.path.join('tests', 'images', '%s.img' % test)
+        rclass = directory.get_radio(test)
+        for device in _get_sub_devices(rclass, image):
+            class_name = 'TestCase_%s' % (
+                filter(lambda c: c.isalnum(),
+                       device.get_name()))
+            tc = TestAdapterMeta(
+                class_name, (TestAdapter,), dict(RADIO_CLASS=device,
+                                                 SOURCE_IMAGE=image))
+        suite.addTests(loader.loadTestsFromTestCase(tc))
+
+    return suite
