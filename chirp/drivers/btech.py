dac01fe269e5a2db9708527ca777f26359cc8574
Dan Smith <dsmith@danplanet.com>
2019-02-13 12:01:49 -0800

    [py3] 2to3 of btech.py
    Not tested with real hardware
    
    #495

--- chirp/drivers/btech.py
+++ chirp/drivers/btech.py
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import time
-import struct
 import logging
 
 LOG = logging.getLogger(__name__)
@@ -28,6 +27,7 @@ from chirp.settings import RadioSettingGroup, RadioSetting, \
     RadioSettingValueBoolean, RadioSettingValueList, \
     RadioSettingValueString, RadioSettingValueInteger, \
     RadioSettingValueFloat, RadioSettings, InvalidValueError
+from chirp.util import StringStruct as struct
 from textwrap import dedent
 
 # A note about the memmory in these radios
@@ -403,7 +403,7 @@ def _start_clone_mode(radio, status):
 
     except errors.RadioError:
         raise
-    except Exception, e:
+    except Exception as e:
         raise errors.RadioError("Error sending Magic to radio:\n%s" % e)
 
 
@@ -768,7 +768,7 @@ class BTechMobileCommon(chirp_common.CloneModeRadio,
             _upload(self)
         except errors.RadioError:
             raise
-        except Exception, e:
+        except Exception as e:
             raise errors.RadioError("Error: %s" % e)
 
     def get_raw_memory(self, number):
@@ -2790,7 +2790,7 @@ class BTechMobileCommon(chirp_common.CloneModeRadio,
                     elif element.value.get_mutable():
                         LOG.debug("Setting %s = %s" % (setting, element.value))
                         setattr(obj, setting, element.value)
-                except Exception, e:
+                except Exception as e:
                     LOG.debug(element.get_name())
                     raise
 
