135ac167b89a50d6a18d1a1da9f5c271a1439c51
Dan Smith <dsmith@danplanet.com>
2019-02-16 12:19:21 -0800

    Fix unittest adapter class
    The adapter was ignoring tests that collect and return multiple failures at once,
    so any CopyAll failures were ignored. This makes the adapter just pick off the first
    one and raise it.
    
    Found while reviewing the driver for #4787

--- tests/__init__.py
+++ tests/__init__.py
@@ -37,7 +37,11 @@ class TestAdapter(unittest.TestCase):
         testcase = test(tw)
         testcase.prepare()
         try:
-            testcase.run()
+            failures = testcase.run()
+            if failures:
+                raise failures[0]
+        except run_tests.TestCrashError as e:
+            raise e.get_original_exception()
         except run_tests.TestSkippedError as e:
             raise unittest.SkipTest(str(e))
         finally:
