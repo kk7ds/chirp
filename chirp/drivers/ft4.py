5b39a606bf76315487f8c10d9011f65464a403e8
DanClemmensen <DanClemmensen@gmail.com>
2019-03-15 16:53:32 -0700

    [ft4] add "+-/" to CHARSET [#6605] Fixes: #6605

--- chirp/drivers/ft4.py
+++ chirp/drivers/ft4.py
@@ -530,7 +530,7 @@ for name in chirp_common.CROSS_MODES:
 DTMF_CHARS = "0123456789ABCD*#- "
 CW_ID_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 PASSWD_CHARS = "0123456789"
-CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz*# "
+CHARSET = CW_ID_CHARS + "abcdefghijklmnopqurtuvwxyz*#+-/"
 PMSNAMES = ["%s%02d" % (c, i) for i in range(1, 11) for c in ('L', 'U')]
 
 # Four separate arrays of special channel mems.
