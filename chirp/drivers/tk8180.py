ad4241d1d6ff5739f943802e2fdf45f3965d5784
Dan Smith <dsmith@danplanet.com>
2020-04-08 16:10:56 -0700

    [tk3180] Fix DCS squelch encoding
    The value we set in memory was slightly different from how the
    OEM software writes it, although the OEM software decodes it properly.
    With our encoding the radio would not actually transmit the DCS code,
    rendering it ineffective.
    
    Fixes #7777

--- chirp/drivers/tk8180.py
+++ chirp/drivers/tk8180.py
@@ -650,7 +650,7 @@ class KenwoodTKx180Radio(chirp_common.CloneModeRadio):
             return int(val * 10)
         elif mode == 'DTCS':
             code = int('%i' % val, 8)
-            code |= 0x2000
+            code |= 0x2800
             if pol == 'R':
                 code |= 0x8000
             return code
