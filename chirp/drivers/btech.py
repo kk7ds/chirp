bcde2c49d7733389893d3335dbe42291a6bfd7f8
Jim Unroe <rock.unroe@gmail.com>
2022-01-14 16:07:39 -0800

    [QB25] Add additional MCU Version for Radioddity QB25
    This patch adds an additional MCU version (VC9204) for the
    Radioddity QB25 (QYT KT7900D variant).
    
    Fixes #9687

--- chirp/drivers/btech.py
+++ chirp/drivers/btech.py
@@ -249,6 +249,7 @@ KT7900D_fp3 = "VC4114"
 KT7900D_fp4 = "VC4104"
 KT7900D_fp5 = "VC4254"
 KT7900D_fp6 = "VC5264"
+KT7900D_fp7 = "VC9204"
 
 # QB25 (quad band) - a clone of KT7900D
 QB25_fp = "QB-25"
@@ -4021,7 +4022,7 @@ class KT7900D(BTechColor):
     _350_range = (350000000, 371000000)
     _magic = MSTRING_KT8900D
     _fileid = [KT7900D_fp, KT7900D_fp1, KT7900D_fp2, KT7900D_fp3, KT7900D_fp4,
-               KT7900D_fp5, KT7900D_fp6, QB25_fp, ]
+               KT7900D_fp5, KT7900D_fp6, KT7900D_fp7, QB25_fp, ]
     # Clones
     ALIASES = [SKT8900D, QB25, ]
 
