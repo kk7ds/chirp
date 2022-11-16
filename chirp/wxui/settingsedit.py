e0abe87f50bd4a522cea6090d122888aba49888d
Dan Smith <dsmith@danplanet.com>
2022-11-15 18:11:17 -0800

    Properly handle immutable settings
    This makes immutable settings insensitive to input in the UI, and
    avoids trying to set them on update.

--- chirp/wxui/settingsedit.py
+++ chirp/wxui/settingsedit.py
@@ -66,6 +66,10 @@ class ChirpSettingsEdit(common.ChirpEditor):
             for i in range(self._group_control.GetPageCount()):
                 page = self._group_control.GetPage(i)
                 for name, (setting, val) in page.get_setting_values().items():
+                    if not setting.value.get_mutable():
+                        LOG.debug('Skipping immutable setting %r' % (
+                            setting.get_name()))
+                        continue
                     LOG.debug('Setting %s:%s=%r' % (page.name,
                                                     setting.get_name(),
                                                     val))
