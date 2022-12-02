d476d7f786c3a064b5f494a147cc9d364e149386
Dan Smith <dsmith@danplanet.com>
2022-12-01 16:16:50 -0800

    Set app icon and cleanup stock configs
    We have always had this odd arrangement where we force copy stock
    config files into the user's own directory and then need to be
    careful to never overwrite them (but then we can't update them).
    This changes our behavior to load from both locations, prioritizing
    the user's configs if they exist with the same name.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 from chirp import CHIRP_VERSION
 
 desktop_files = glob("share/*.desktop")
-image_files = glob("images/*")
+image_files = glob('share/*.png')
 stock_configs = glob("stock_configs/*")
 
 setup(name='chirp',
@@ -14,15 +14,8 @@ setup(name='chirp',
       version=CHIRP_VERSION,
       url='https://chirp.danplanet.com',
       python_requires=">=3.3,<4",
-      install_required=['wxPython', 'serial'],
+      install_required=['wxPython', 'serial', 'six', 'future'],
       entry_points={
           'console_scripts': ["chirp=chirp.wxui:chirpmain"],
       },
-      data_files=[('share/applications', desktop_files),
-                  ('share/chirp/images', image_files),
-                  ('share/doc/chirp', ['COPYING']),
-                  ('share/pixmaps', ['share/chirp.png',
-                                     'share/welcome_screen.png']),
-                  ('share/man/man1', ["share/chirpw.1"]),
-                  ('share/chirp/stock_configs', stock_configs),
-                  ])
+      )
