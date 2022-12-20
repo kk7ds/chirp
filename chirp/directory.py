20896a9eea3e720b58cd13f843fbee8da2f5f609
Dan Smith <dsmith@danplanet.com>
2022-12-20 14:45:57 -0800

    Remove unused URI stuff from directory

--- chirp/directory.py
+++ chirp/directory.py
@@ -24,7 +24,6 @@ import sys
 import six
 
 from chirp import chirp_common, util, errors
-from chirp.sources import radioreference
 
 LOG = logging.getLogger(__name__)
 
@@ -125,19 +124,6 @@ MODEL_COMPAT = {
 
 def get_radio_by_image(image_file):
     """Attempt to get the radio class that owns @image_file"""
-    if image_file.startswith("radioreference://"):
-        _, _, zipcode, username, password, country = image_file.split("/", 5)
-        rr = radioreference.RadioReferenceRadio(None)
-        rr.set_params(zipcode, username, password, country)
-        return rr
-
-    # FIXME: Disable rfinder until the module is fixed
-    if image_file.startswith("rfinder://") and False:
-        _, _, email, passwd, lat, lon, miles = image_file.split("/")
-        rf = rfinder.RFinderRadio(None)
-        rf.set_params((float(lat), float(lon)), int(miles), email, passwd)
-        return rf
-
     if os.path.exists(image_file):
         with open(image_file, "rb") as f:
             filedata = f.read()
