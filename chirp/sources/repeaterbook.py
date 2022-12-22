b055d406538e34521b44a2f9744a7178416f16da
Dan Smith <dsmith@danplanet.com>
2022-12-21 17:37:10 -0800

    Use the formal repeaterbook API
    CHIRP has long used a custom API that returns CHIRP-compliant CSV.
    This limits the detail we can get about repeaters, and also requires
    repeaterbook to handle quirks like CHIRP's tone modes. This moves
    us to use the formal API that others use, which allows us to sort
    by distance from lat/lon, etc. It also caches the DB exports locally
    for 30 days at a time, making re-queries instantaneous.

--- chirp/sources/repeaterbook.py
+++ chirp/sources/repeaterbook.py
@@ -1,13 +1,93 @@
+import datetime
+import json
 import logging
+import math
 import os
 import tempfile
 
 import requests
 
+from chirp import chirp_common
 from chirp.drivers import generic_csv
+from chirp import platform as chirp_platform
 from chirp.sources import base
+from chirp.wxui import fips
 
 LOG = logging.getLogger(__name__)
+
+COUNTRIES = (
+    'United States',
+    'Canada',
+    'Mexico',
+)
+STATES = list(fips.FIPS_STATES.keys())
+STATES.extend([
+    "Aguascalientes",
+    "Baja California Sur",
+    "Baja California",
+    "Campeche",
+    "Chiapas",
+    "Chihuahua",
+    "Coahuila",
+    "Colima",
+    "Durango",
+    "Guanajuato",
+    "Guerrero",
+    "Hidalgo",
+    "Jalisco",
+    "Mexico City",
+    "Mexico",
+    "Michoacán",
+    "Morelos",
+    "Nayarit",
+    "Nuevo Leon",
+    "Puebla",
+    "Queretaro",
+    "Quintana Roo",
+    "San Luis Potosi",
+    "Sinaloa",
+    "Sonora",
+    "Tabasco",
+    "Tamaulipas",
+    "Tlaxcala",
+    "Veracruz",
+    "Yucatan",
+    "Zacatecas",
+])
+
+def parse_tone(val):
+    if val.startswith('D'):
+        mode = 'DTCS'
+        val = int(val[1:])
+    elif '.' in val:
+        mode = 'Tone'
+        val = float(val)
+    elif val in ('CSQ', 'Restricted'):
+        val = mode = None
+    elif val:
+        LOG.warning('Unsupported PL format: %r' % val)
+        val = mode = None
+    else:
+        val = mode = None
+    return mode, val
+
+
+def distance(lat_a, lon_a, lat_b, lon_b):
+    lat_a = math.radians(lat_a)
+    lon_a = math.radians(lon_a)
+
+    lat_b = math.radians(lat_b)
+    lon_b = math.radians(lon_b)
+
+    earth_radius_km = 6371
+
+    dlon = lon_b - lon_a
+    dlat = lat_b - lat_a
+
+    a = math.sin(dlat / 2)**2 + math.cos(lat_a) * \
+        math.cos(lat_b) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return earth_radius_km * c
 
 
 class RepeaterBook(base.NetworkResultRadio):
@@ -16,49 +96,131 @@ class RepeaterBook(base.NetworkResultRadio):
     def get_label(self):
         return 'RepeaterBook'
 
-    def do_fetch(self, status, params):
-        status.send_status('Querying', 10)
-
-        r = requests.get('http://chirp.danplanet.com/%s' % params.pop('_url'),
-                         params=params,
+    def get_data(self, status, country, state):
+        # Ideally we would be able to pull the whole database, but right
+        # now this is limited to 3500 results, so we need to filter and
+        # cache by state to stay under that limit.
+        fn = 'rb-%s-%s.json' % (country.lower().replace(' ', '_'),
+                                state.lower().replace(' ', '_'))
+        db_dir = chirp_platform.get_platform().config_file('repeaterbook')
+        try:
+            os.mkdir(db_dir)
+        except FileExistsError:
+            pass
+        except Exception as e:
+            LOG.exception('Failed to create %s: %s' % (db_dir, e))
+            status.set_fail('Internal error - check log')
+            return
+        data_file = os.path.join(db_dir, fn)
+        try:
+            modified = os.path.getmtime(data_file)
+        except FileNotFoundError:
+            modified = 0
+        modified_dt = datetime.datetime.fromtimestamp(modified)
+        interval = datetime.timedelta(days=30)
+        if datetime.datetime.now() - modified_dt < interval:
+            return data_file
+        if modified == 0:
+            LOG.debug('RepeaterBook database %s not cached' % fn)
+        else:
+            LOG.debug('RepeaterBook database %s too old: %s' % modified_dt)
+        r = requests.get('https://www.repeaterbook.com/api/export.php',
+                         params={'country': country,
+                                 'state': state},
                          headers=base.HEADERS,
                          stream=True)
         if r.status_code != 200:
-            status.send_fail('Got error code %i from server' % (
-                r.status_code))
+            if modified:
+                status.send_status('Using cached data', 50)
+            status.send_fail('Got error code %i from server' % r.status_code)
+            return
+        tmp = data_file + '.tmp'
+        chunk_size = 8192
+        probable_end = 3 << 20
+        counter = 0
+        data = b''
+        with open(tmp, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+                data += chunk
+                counter += len(chunk)
+                status.send_status('Downloading', counter / probable_end * 50)
+        try:
+            results = json.loads(data)
+        except Exception as e:
+            LOG.exception('Query failed: %s' % e)
+            status.send_fail('Query failed')
             return
 
-        status.send_status('Downloading', 20)
-
-        size = 0
-        chunks = 0
-
-        result_file = tempfile.NamedTemporaryFile(
-            prefix='repeaterbook-',
-            suffix='.csv').name
-
-        LOG.debug('Writing to %s' % result_file)
-        with open(result_file, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                size += len(chunk)
-                chunks += 1
-                status.send_status('Read %iKiB' % (size // 1024),
-                                 20 + max(chunks * 10, 80))
-                f.write(chunk)
-
-        if size <= 105:
+        if results['count']:
+            os.rename(tmp, data_file)
+        else:
+            os.remove(tmp)
             status.send_fail('No results!')
             return
 
-        radio = generic_csv.CSVRadio(None)
-        radio.load(result_file)
-        f = radio.get_features()
-        self._memories = [radio.get_memory(i)
-                          for i in range(0, f.memory_bounds[1])
-                          if not radio.get_memory(i).empty]
-        try:
-            os.remove(result_file)
-        except OSError:
-            pass
+        status.send_status('Download complete', 50)
+        return data_file
+
+    def do_fetch(self, status, params):
+        lat = float(params.pop('lat') or 0)
+        lon = float(params.pop('lon') or 0)
+        dist = int(params.pop('dist') or 0)
+        data_file = self.get_data(status,
+                                  params.pop('country'),
+                                  params.pop('state'))
+        if not data_file:
+            return
+
+        status.send_status('Parsing', 50)
+
+        def sorter(item):
+            if lat == 0 and lon == 0:
+                # No sort if not provided
+                return 0
+            return distance(lat, lon,
+                            float(item.get('Lat', 0)),
+                            float(item.get('Long', 0)))
+
+        i = 0
+        for item in sorted(json.loads(open(data_file, 'rb').read())['results'],
+                           key=sorter):
+            if not item:
+                continue
+            if item['Operational Status'] != 'On-air':
+                continue
+            if dist and lat and lon and (
+                distance(lat, lon,
+                         float(item.get('Lat') or 0),
+                         float(item.get('Long') or 0)) > dist):
+                continue
+            m = chirp_common.Memory()
+            m.number = i
+            m.freq = chirp_common.parse_freq(item['Frequency'])
+            txf = chirp_common.parse_freq(item['Input Freq'])
+            chirp_common.split_to_offset(m, m.freq, txf)
+            txm, tx = parse_tone(item['PL'])
+            rxm, rx = parse_tone(item['TSQ'])
+            chirp_common.split_tone_decode(m, (txm, tx, 'N'), (rxm, rx, 'N'))
+            if item['DMR'] == 'Yes':
+                m.mode = 'DMR'
+            elif item['D-Star'] == 'Yes':
+                m.mode = 'DV'
+            elif item['System Fusion'] == 'Yes':
+                # FIXME: need YSF mode
+                # m.mode = 'YSF'
+                pass
+            elif item['FM Analog'] == 'Yes':
+                m.mode = 'FM'
+            else:
+                LOG.warning('Unable to determine mode for repeater %s' % (
+                    item['Rptr ID']))
+                continue
+            m.comment = \
+                '%(Callsign)s near %(Nearest City)s %(State)s %(Use)s' % (
+                    item)
+            m.name = item['Landmark'] or item['Callsign']
+            i += 1
+            self._memories.append(m)
 
         status.send_end()
--- chirp/wxui/query_sources.py
+++ chirp/wxui/query_sources.py
@@ -157,16 +157,6 @@ class QuerySourceDialog(wx.Dialog):
 
 class RepeaterBookQueryDialog(QuerySourceDialog):
     NAME = 'Repeaterbook'
-    RB_BANDS = {
-        _('--All--'):                  0,
-        _('10 meters (29MHz)'):        29,
-        _('6 meters (54MHz)'):         5,
-        _('2 meters (144MHz)'):        14,
-        _('1.25 meters (220MHz)'):     22,
-        _('70 centimeters (440MHz)'):  4,
-        _('33 centimeters (900MHz)'):  9,
-        _('23 centimeters (1.2GHz)'):  12,
-    }
 
     def get_info(self):
         return (
@@ -177,161 +167,72 @@ class RepeaterBookQueryDialog(QuerySourceDialog):
         return 'https://repeaterbook.com'
 
     def build(self):
-        self.tabs = wx.Notebook(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(vbox)
-        vbox.Add(self.tabs, proportion=1, border=10,
+        panel = wx.Panel(self)
+        vbox.Add(panel, proportion=1, border=10,
                  flag=wx.EXPAND | wx.BOTTOM)
+        grid = wx.FlexGridSizer(2, 5, 0)
+        grid.AddGrowableCol(1)
+        panel.SetSizer(grid)
 
-        self.tabs.InsertPage(0, self._build_political(self.tabs),
-                             _('Political'))
-        self.tabs.InsertPage(1, self._build_proximity(self.tabs),
-                             _('Proximity'))
+        self._country = wx.Choice(panel, choices=repeaterbook.COUNTRIES)
+        prev = CONF.get('country', 'repeaterbook')
+        if prev and prev in repeaterbook.COUNTRIES:
+            self._country.SetStringSelection(prev)
+        self._add_grid(grid, _('Country'), self._country)
+
+        self._state = wx.Choice(panel, choices=sorted(repeaterbook.STATES))
+        prev = CONF.get('state', 'repeaterbook')
+        if prev and prev in repeaterbook.STATES:
+            self._state.SetStringSelection(prev)
+        self._add_grid(grid, _('State/Province'), self._state)
+
+        self._lat = wx.TextCtrl(panel,
+                                value=CONF.get('lat', 'repeaterbook') or '')
+        self._lat.SetHint(_('Optional'))
+        self._lat.SetToolTip(_('If set, sort results by distance from '
+                               'these coordinates'))
+        self._lon = wx.TextCtrl(panel,
+                                value=CONF.get('lon', 'repeaterbook') or '')
+        self._lon.SetHint(_('Optional'))
+        self._lon.SetToolTip(_('If set, sort results by distance from '
+                               'these coordinates'))
+        self._add_grid(grid, _('Latitude'), self._lat)
+        self._add_grid(grid, _('Longitude'), self._lon)
+
+        self._dist = wx.TextCtrl(panel,
+                                 value=CONF.get('dist', 'repeaterbook') or '')
+        self._dist.SetHint(_('Optional'))
+        self._dist.SetToolTip(_('Limit results to this distance (km) from '
+                                'coordinates'))
+        self._add_grid(grid, _('Distance'), self._dist)
+
         self.Layout()
         return vbox
-
-    def _build_band_choice(self, parent):
-        return wx.Choice(parent, choices=list(self.RB_BANDS.keys()))
 
     def _add_grid(self, grid, label, widget):
         grid.Add(wx.StaticText(widget.GetParent(), label=label),
                  border=20, flag=wx.ALIGN_CENTER | wx.RIGHT | wx.LEFT)
         grid.Add(widget, 1, border=20, flag=wx.EXPAND | wx.RIGHT | wx.LEFT)
 
-    def _build_political(self, parent):
-        panel = wx.Panel(parent)
-        grid = wx.FlexGridSizer(2, 5, 0)
-        grid.AddGrowableCol(1)
-
-        self._state = wx.Choice(panel, choices=sorted(fips.FIPS_STATES.keys()))
-        self.Bind(wx.EVT_CHOICE, self._selected_state, self._state)
-        self._county = wx.Choice(panel, choices=['--All--'])
-        self.Bind(wx.EVT_CHOICE, self._selected_county, self._county)
-        self._pol_band = self._build_band_choice(panel)
-        self.Bind(wx.EVT_CHOICE, self._selected_band, self._pol_band)
-
-        self._fips_to_state = {str(v): k for k, v in fips.FIPS_STATES.items()}
-
-        current_state = CONF.get('state', 'repeaterbook')
-        current_county = CONF.get('county', 'repeaterbook')
-        if not current_state:
-            current_state = '1'
-        self._state.SetSelection(
-            self._state.FindString(self._fips_to_state[current_state]))
-        self._selected_state_name(self._fips_to_state[current_state])
-
-        try:
-            _fips_to_county = {
-                str(v): k
-                for k, v in fips.FIPS_COUNTIES[int(current_state)].items()}
-            self._county.SetSelection(
-                self._county.FindString(_fips_to_county[current_county]))
-        except Exception:
-            if current_county:
-                LOG.warning('Missing county code %r in state %r' % (
-                    current_county,
-                    current_state))
-
-        self._add_grid(grid, _('State'), self._state)
-        self._add_grid(grid, _('County'), self._county)
-        self._add_grid(grid, _('Band'), self._pol_band)
-
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        vbox.Add(grid, proportion=0, flag=wx.EXPAND | wx.TOP | wx.BOTTOM,
-                 border=20)
-        panel.SetSizer(vbox)
-        return panel
-
-    def _selected_state(self, event):
-        self._selected_state_name(event.GetString())
-
-    def _selected_state_name(self, state):
-        counties = sorted(fips.FIPS_COUNTIES[fips.FIPS_STATES[state]].keys())
-        self._county.Set(counties)
-        CONF.set('state', str(fips.FIPS_STATES[state]), 'repeaterbook')
-        self._selected_county_name(counties[0])
-
-    def _selected_county(self, event):
-        self._selected_county_name(event.GetString())
-
-    def _selected_county_name(self, county):
-        state = CONF.get('state', 'repeaterbook')
-        try:
-            state = int(state)
-        except ValueError:
-            pass
-        county_code = str(fips.FIPS_COUNTIES[state][county])
-        if county_code == '%':
-            CONF.remove_option('county', 'repeaterbook')
-        else:
-            CONF.set('county', county_code, 'repeaterbook')
-
-    def _build_proximity(self, parent):
-        panel = wx.Panel(parent)
-        grid = wx.FlexGridSizer(2, 5, 0)
-        grid.AddGrowableCol(1)
-
-        self._location = wx.TextCtrl(panel)
-        self._location.AppendText(CONF.get('location', 'repeaterbook') or '')
-        self.Bind(wx.EVT_TEXT, self._selected_location, self._location)
-        self._distance = wx.TextCtrl(panel)
-        self._distance.AppendText(CONF.get('distance', 'repeaterbook') or '')
-        self.Bind(wx.EVT_TEXT, self._selected_distance, self._distance)
-        self._prox_band = self._build_band_choice(panel)
-        self.Bind(wx.EVT_CHOICE, self._selected_band, self._prox_band)
-
-        self._add_grid(grid, _('Location'), self._location)
-        self._add_grid(grid, _('Distance'), self._distance)
-        self._add_grid(grid, _('Band'), self._prox_band)
-
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        vbox.Add(grid, flag=wx.EXPAND | wx.TOP | wx.BOTTOM,
-                 border=20)
-        panel.SetSizer(vbox)
-        return panel
-
-    def _selected_location(self, event):
-        CONF.set('location', event.GetString(), 'repeaterbook')
-
-    def _selected_distance(self, event):
-        CONF.set('distance', event.GetString(), 'repeaterbook')
-
-    def _selected_band(self, event):
-        CONF.set('band', str(self.RB_BANDS[event.GetString()]), 'repeaterbook')
-
     def do_query(self):
+        CONF.set('lat', self._lat.GetValue(), 'repeaterbook')
+        CONF.set('lon', self._lon.GetValue(), 'repeaterbook')
+        CONF.set('dist', self._dist.GetValue(), 'repeaterbook')
+        CONF.set('state', self._state.GetStringSelection(), 'repeaterbook')
+        CONF.set('country', self._country.GetStringSelection(), 'repeaterbook')
         self.result_radio = repeaterbook.RepeaterBook()
         super().do_query()
 
     def get_params(self):
-        page = self.tabs.GetSelection()
-        if page == 0:
-            # Political
-            return {
-                '_url': 'query/rb/1.0/chirp',
-                'state_id': CONF.get('state', 'repeaterbook'),
-                'county_id': CONF.get('county', 'repeaterbook') or '%',
-                'func': 'default',
-                'band': CONF.get('band', 'repeaterbook') or '%',
-                'freq': '%',
-                'band6': '%',
-                'loc': '%',
-                'status_id': '%',
-                'features': '%',
-                'coverage': '%',
-                'use': '%',
-            }
-        elif page == 1:
-            # Proximity
-            try:
-                return {
-                    '_url': 'query/rb/1.0/app_direct',
-                    'loc': CONF.get('location', 'repeaterbook'),
-                    'dist': int(CONF.get('distance', 'repeaterbook')),
-                    'band': CONF.get('band', 'repeaterbook') or '%',
-                }
-            except (TypeError, ValueError):
-                raise Exception(_('Distance must be a number!'))
+        return {
+            'country': self._country.GetStringSelection(),
+            'state': self._state.GetStringSelection(),
+            'lat': self._lat.GetValue(),
+            'lon': self._lon.GetValue(),
+            'dist': self._dist.GetValue(),
+        }
 
 
 class DMRMARCQueryDialog(QuerySourceDialog):
