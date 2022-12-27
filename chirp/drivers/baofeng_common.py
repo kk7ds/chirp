9fcab01d7713b9e60803883229f2803dcdd3740e
Jim Unroe <rock.unroe@gmail.com>
2022-12-27 17:06:38 -0500

    Driver update: baofeng_common.py
    * Switch frequency tables to Hz based

--- chirp/drivers/baofeng_common.py
+++ chirp/drivers/baofeng_common.py
@@ -315,12 +315,12 @@ class BaofengCommonHT(chirp_common.CloneModeRadio,
     MODEL = ""
     IDENT = ""
 
-    GMRS_FREQS1 = [462.5625, 462.5875, 462.6125, 462.6375, 462.6625,
-                   462.6875, 462.7125]
-    GMRS_FREQS2 = [467.5625, 467.5875, 467.6125, 467.6375, 467.6625,
-                   467.6875, 467.7125]
-    GMRS_FREQS3 = [462.5500, 462.5750, 462.6000, 462.6250, 462.6500,
-                   462.6750, 462.7000, 462.7250]
+    GMRS_FREQS1 = [462562500, 462587500, 462612500, 462637500, 462662500,
+                   462687500, 462712500]
+    GMRS_FREQS2 = [467562500, 467587500, 467612500, 467637500, 467662500,
+                   467687500, 467712500]
+    GMRS_FREQS3 = [462550000, 462575000, 462600000, 462625000, 462650000,
+                   462675000, 462700000, 462725000]
     GMRS_FREQS = GMRS_FREQS1 + GMRS_FREQS2 + GMRS_FREQS3 * 2
     _gmrs = False
 
@@ -527,7 +527,7 @@ class BaofengCommonHT(chirp_common.CloneModeRadio,
 
         if self._gmrs:
             if mem.number >= 1 and mem.number <= 30:
-                GMRS_FREQ = int(self.GMRS_FREQS[mem.number - 1] * 1000000)
+                GMRS_FREQ = self.GMRS_FREQS[mem.number - 1]
                 mem.freq = GMRS_FREQ
                 if mem.number <= 22:
                     mem.duplex = ''
@@ -538,12 +538,12 @@ class BaofengCommonHT(chirp_common.CloneModeRadio,
                 if mem.number > 22:
                     mem.duplex = '+'
                     mem.offset = 5000000
-            elif float(mem.freq) / 1000000 in self.GMRS_FREQS:
-                if float(mem.freq) / 1000000 in self.GMRS_FREQS2:
+            elif mem.freq in self.GMRS_FREQS:
+                if mem.freq in self.GMRS_FREQS2:
                     mem.offset = 0
                     mem.mode = "NFM"
                     mem.power = self.POWER_LEVELS[2]
-                if float(mem.freq) / 1000000 in self.GMRS_FREQS3:
+                if mem.freq in self.GMRS_FREQS3:
                     if mem.duplex == '+':
                         mem.offset = 5000000
                     else:
