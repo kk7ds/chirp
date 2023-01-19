a28a6d894ffbaad092c6bfe0c7a67019c3ce04ce
Dan Smith <dsmith@danplanet.com>
2023-01-18 16:07:03 -0800

    Fix FT-817/857/897 immutable comment
    These drivers have comment marked as immutable because they don't
    support it, but that prevents us from being able to support it
    via the generic code.
    
    Fixes: #10289

--- chirp/drivers/ft857.py
+++ chirp/drivers/ft857.py
@@ -1150,7 +1150,7 @@ class FT857USRadio(FT857Radio):
         mem.immutable = ["number", "rtone", "ctone",
                          "extd_number", "name", "dtcs", "tmode", "cross_mode",
                          "dtcs_polarity", "power", "duplex", "offset",
-                         "comment", "empty"]
+                         "empty"]
 
         return mem
 
