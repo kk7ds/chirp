1a17d6ea76f2bb0230e98efd1a277c1d0236202a
Dan Smith <dsmith@danplanet.com>
2023-01-18 16:05:58 -0800

    Fix 817ND US variant 60m channels
    This is actually the same as bug #10209 I just didn't realize it was
    present in the 817 as well.

--- chirp/drivers/ft817.py
+++ chirp/drivers/ft817.py
@@ -1190,9 +1190,9 @@ class FT817NDUSRadio(FT817Radio):
         self._set_memory(mem, _mem)
 
     def get_memory(self, number):
-        if number in list(self.SPECIAL_60M.keys()):
+        if number in self.SPECIAL_60M.keys():
             return self._get_special_60m(number)
-        elif (number < 0 and
+        elif (isinstance(number, int) and number < 0 and
               self.SPECIAL_MEMORIES_REV[number] in
               list(self.SPECIAL_60M.keys())):
             # I can't stop delete operation from losing extd_number but
