fa52f57929c646169a3861eb77f850e0423bf7ea
Dan Smith <dsmith@danplanet.com>
2023-02-28 21:40:38 -0800

    Allow loaded modules to be reloaded
    This changes how we load modules in the first place to import them
    in a more normal way such that they get the proper file association,
    etc. It also loads them into a chirp.loaded namespace. Finally, it
    tags them as loaded and makes our reload procedure just load them
    again instead of the usual importlib.reload() which doesn't seem to
    work regardless of how we load them.

--- chirp/wxui/main.py
+++ chirp/wxui/main.py
@@ -1184,10 +1184,14 @@ class ChirpMain(wx.Frame):
 
         # Reload the actual module
         module = sys.modules[orig_rclass.__module__]
-        LOG.warning('Going to reload %s' % module)
-        directory.enable_reregistrations()
-        import importlib
-        importlib.reload(module)
+        if hasattr(module, '_was_loaded'):
+            LOG.warning('Reloading loaded module %s' % module)
+            self.load_module(module.__file__)
+        else:
+            LOG.warning('Going to reload %s' % module)
+            directory.enable_reregistrations()
+            import importlib
+            importlib.reload(module)
 
         # Grab a new reference to the updated module and pick out the
         # radio class from it.
@@ -1249,10 +1253,17 @@ class ChirpMain(wx.Frame):
         sha = hashlib.sha256()
         sha.update(code.encode())
         LOG.info('Loading module %s SHA256 %s' % (filename, sha.hexdigest()))
-        pyc = compile(code, filename, 'exec')
-        # See this for why:
-        # http://stackoverflow.com/questions/2904274/globals-and-locals-in-python-exec
-        exec(pyc, globals(), globals())
+
+        import importlib.util
+        import sys
+        modname = 'chirp.loaded.%s' % os.path.splitext(
+            os.path.basename(filename))[0]
+
+        spec = importlib.util.spec_from_file_location(modname, filename)
+        module = importlib.util.module_from_spec(spec)
+        module._was_loaded = True
+        sys.modules[modname] = module
+        spec.loader.exec_module(module)
 
     def _menu_load_module(self, event):
         formats = ['Python %s (*.py)|*.py' % _('Files'),
