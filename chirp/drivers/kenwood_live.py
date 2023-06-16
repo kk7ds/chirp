kk7ds/chirp

commit 9f5d650890d92e7de08e3b410d185ad264a143ad

Revert to RTS signaling by default
Since the pyserial bug mentioned in 0b4dc922, we have had RTS enabled
for all radios, even though it wasn't intentional. Turns out quite a
lot of radios require that, and we didn't have them marked as
HARDWARE_FLOW=True because it was already enabled. This reverts
changing the default to False, explicitly enables it for all radios
except for those that don't want it. Also mark the icomciv driver as
disabled explicitly. Mark parent KenwoodLive as wanting hardware flow
explicitly (even though it's the default) since so many of those
require it, in case the default were to change later.

Related to #10515

chirp/drivers/kenwood_live.py
@@ -174,6 +174,9 @@ class KenwoodLiveRadio(chirp_common.LiveRadio):
     VENDOR = "Kenwood"
     MODEL = ""
     NEEDS_COMPAT_SERIAL = False
+    # Lots of Kenwood radios actually require RTS, even some of the ones with
+    # USB integrated
+    HARDWARE_FLOW = True
 
     _vfo = 0
     _upper = 200

chirp/chirp_common.py
@@ -1120,7 +1120,7 @@ class Radio(Alias):
     """Base class for all Radio drivers"""
     BAUD_RATE = 9600
     # Whether or not we should assert RTS (and use RTS/CTS flow control)
-    HARDWARE_FLOW = False
+    HARDWARE_FLOW = True
     # Whether or not we should assert DTR when opening the serial port
     WANTS_DTR = True
     ALIASES = []

chirp/drivers/icomciv.py
@@ -338,6 +338,8 @@ class IcomCIVRadio(icf.IcomLiveRadio):
     BAUD_RATE = 19200
     NEEDS_COMPAT_SERIAL = False
     MODEL = "CIV Radio"
+    # RTS is interpreted as "transmit now" on some interface boxes for these
+    HARDWARE_FLOW = False
     _model = "\x00"
     _template = 0
 

commit 87c0e331aeabce51152e991b6ca285f1cf752df0

Disable HARDWARE_FLOW for Kenwood TH-F6
The TH-F6 driver appears to have been broken since Commit 9f5d6508,
which defaulted this value to True.

chirp/drivers/kenwood_live.py
@@ -901,6 +901,7 @@ THF6A_DUPLEX[3] = "split"
 class THF6ARadio(KenwoodLiveRadio):
     """Kenwood TH-F6"""
     MODEL = "TH-F6"
+    HARDWARE_FLOW = False
 
     _charset = chirp_common.CHARSET_ASCII
     _upper = 399