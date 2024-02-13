9767d45683c10413beae4a20685dce9f7f9e2505
Dan Smith <dsmith@danplanet.com>
2024-02-12 19:57:45 -0800

    retevis_rb28: Optimize the PMR constant tuples
    Same as we did for chirp_common recently.

--- chirp/drivers/retevis_rb28.py
+++ chirp/drivers/retevis_rb28.py
@@ -140,14 +140,17 @@ VOICE_LIST = ["Off", "English"]
 VOXD_LIST = ["0.5", "1.0", "1.5", "2.0", "2.5", "3.0"]
 VOXL_LIST = ["OFF"] + ["%s" % x for x in range(1, 10)]
 
-PMR_TONES = list(chirp_common.TONES)
-[PMR_TONES.remove(x) for x in [69.3, 159.8, 165.5, 171.3, 177.3, 183.5,
-                               189.9, 196.6, 199.5, 206.5, 229.1, 254.1]]
+PMR_TONES = tuple(
+    set(chirp_common.TONES) - set([69.3, 159.8, 165.5, 171.3, 177.3,
+                                   183.5, 189.9, 196.6, 199.5, 206.5,
+                                   229.1, 254.1]))
 
-PMR_DTCS_CODES = list(chirp_common.DTCS_CODES)
-[PMR_DTCS_CODES.remove(x) for x in [36,  53, 122, 145, 212, 225, 246,
-                                    252, 255, 266, 274, 325, 332, 356,
-                                    446, 452, 454, 455, 462, 523, 526]]
+PMR_DTCS_CODES = tuple(
+    set(chirp_common.DTCS_CODES) - set([36,  53, 122, 145, 212,
+                                        225, 246, 252, 255, 266,
+                                        274, 325, 332, 356, 446,
+                                        452, 454, 455, 462, 523,
+                                        526]))
 
 
 def _enter_programming_mode(radio):
