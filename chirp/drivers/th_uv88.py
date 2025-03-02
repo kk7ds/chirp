59b5d4edff09d545f89ad7ef2302bcef1610b5fa
Dan Smith <dsmith@danplanet.com>
2025-03-02 10:36:41 -0800

    th_uv88: Add Dual Wait settings for RA89,P2,etc
    Also "Tx Ch Select".
    
    Fixes: #11864

--- chirp/drivers/th_uv88.py
+++ chirp/drivers/th_uv88.py
@@ -24,7 +24,7 @@ from chirp import bitwise, errors, util
 from chirp.settings import RadioSettingGroup, RadioSetting, \
     RadioSettingValueBoolean, RadioSettingValueList, \
     RadioSettingValueString, RadioSettingValueInteger, \
-    RadioSettingValueFloat, RadioSettings
+    RadioSettingValueFloat, RadioSettings, RadioSettingValueMap
 
 LOG = logging.getLogger(__name__)
 
@@ -214,7 +214,15 @@ struct {
      sideKey1:4;          //        side key 1
   u8 sideKey2_long:4,     // 0x1161 side key 2 Long
      sideKey1_long:4;     //        side key 1 Long
-  u8 unknownBytes[9];     // 0x1162 - 0x116A
+  u8 unknown1;
+  u8 dwchan;
+  u8 unknown3;
+  u8 unknown4;
+  u8 unknown5;
+  u8 unknown6;
+  u8 unknown7;
+  u8 unknown8;
+  u8 unknown9;
   u8 manDownTm:4,         // 0x116B manDown Tm
      unk15:3,             //
      manDownSw:1;         //        manDown Sw
@@ -870,13 +878,12 @@ class THUV88Radio(chirp_common.CloneModeRadio):
         group = RadioSettings(basic)
 
         # Menu 02 - TX Channel Select
-        if self._hasLCD:
-            options = ["Last Channel", "Main Channel"]
-            rx = RadioSettingValueList(
-                options, current_index=_settings.txChSelect)
-            rset = RadioSetting("basicsettings.txChSelect",
-                                "Priority Transmit", rx)
-            basic.append(rset)
+        options = ["Last Channel", "Main Channel"]
+        rx = RadioSettingValueList(
+            options, current_index=_settings.txChSelect)
+        rset = RadioSetting("basicsettings.txChSelect",
+                            "Priority Transmit", rx)
+        basic.append(rset)
 
         # Menu 03 - VOX Level
         rx = RadioSettingValueInteger(1, 7, _settings.voxLevel + 1)
@@ -890,11 +897,16 @@ class THUV88Radio(chirp_common.CloneModeRadio):
         basic.append(rset)
 
         # Menu 06 - Dual Wait
-        if self._hasLCD:
-            rx = RadioSettingValueBoolean(_settings.dualWait)
-            rset = RadioSetting("basicsettings.dualWait",
-                                "Dual Wait/Standby", rx)
-            basic.append(rset)
+        rx = RadioSettingValueBoolean(_settings.dualWait)
+        rset = RadioSetting("basicsettings.dualWait",
+                            "Dual Wait/Standby", rx)
+        basic.append(rset)
+
+        if 'dwchan' in _settings:
+            rsv = RadioSettingValueMap([(str(x + 1), x) for x in range(200)],
+                                       _settings.dwchan)
+            rs = RadioSetting('basicsettings.dwchan', 'Dual Wait Channel', rsv)
+            basic.append(rs)
 
         # Menu 07 - LED Mode
         if self._hasLCD:
