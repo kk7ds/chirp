1c9d502a6e5a085c249f4e1f4982ea6ef5d70a9d
Dan Smith <dsmith@danplanet.com>
2026-01-27 15:18:54 -0800

    uvk5: Bless new found firmware versions 7.00.x
    Fixes #12351

--- chirp/drivers/uvk5.py
+++ chirp/drivers/uvk5.py
@@ -2008,7 +2008,7 @@ class UVK5Radio(UVK5RadioBase):
         approved_prefixes = (
             # These are the original OEM firmware versions
             'k5_2.01.', 'app_2.01.', '2.01.', '3.00.',
-            '4.00.', 'k5_4.00.', '5.00.',
+            '4.00.', 'k5_4.00.', '5.00.', '7.00.',
             # This "oneofeeleven" prefix really covers a wide range of
             # firmwares that are user-built, but people report them working
             # fine with the base driver.
