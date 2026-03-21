2976ec5187de96876c821a0aa5e60339a1189856
Dan Smith <dsmith@danplanet.com>
2026-03-21 06:53:57 -0700

    kyd_IP620: Move to common tone model

--- chirp/drivers/kyd_IP620.py
+++ chirp/drivers/kyd_IP620.py
@@ -24,6 +24,7 @@ import struct
 import time
 import logging
 from chirp import util, chirp_common, bitwise, memmap, errors, directory
+from chirp import kenwood_tone
 from chirp.settings import RadioSetting, RadioSettingGroup, \
                 RadioSettingValueList, RadioSettings
 
@@ -34,8 +35,8 @@ IP620_MEM_FORMAT = """
 struct {           // Channel memory structure
   lbcd rx_freq[4]; // RX frequency
   lbcd tx_freq[4]; // TX frequency
-  ul16 rx_tone;    // RX tone
-  ul16 tx_tone;    // TX tone
+  ul16 rxtone; // RX tone
+  ul16 txtone; // TX tone
   u8 unknown_1:4,  // n-a
      busy_loc:2,   // NO-00, Carrier wave-01, SM-10
      n_a:2;        // n-a
@@ -169,6 +170,8 @@ class IP620Radio(chirp_common.CloneModeRadio,
                (0x0000, 0x2000),
               ]
     _memsize = 0x2000
+    _tone_model = kenwood_tone.KenwoodToneModel(
+        dcs_base=0x2800, pol_mask=0x8000, tone_init=0xFFFF, tone_flag=0x0000)
 
     @classmethod
     def match_model(cls, filedata, filename):
@@ -325,48 +328,6 @@ class IP620Radio(chirp_common.CloneModeRadio,
     def get_raw_memory(self, number):
         return repr(self._memobj.memory[number - 1])
 
-    def _get_tone(self, _mem, mem):
-        def _get_dcs(val):
-            code = int("%03o" % (val & 0x07FF))
-            pol = (val & 0x8000) and "R" or "N"
-            return code, pol
-
-        if _mem.tx_tone != 0xFFFF and _mem.tx_tone > 0x2800:
-            tcode, tpol = _get_dcs(_mem.tx_tone)
-            mem.dtcs = tcode
-            txmode = "DTCS"
-        elif _mem.tx_tone != 0xFFFF:
-            mem.rtone = _mem.tx_tone / 10.0
-            txmode = "Tone"
-        else:
-            txmode = ""
-
-        if _mem.rx_tone != 0xFFFF and _mem.rx_tone > 0x2800:
-            rcode, rpol = _get_dcs(_mem.rx_tone)
-            mem.rx_dtcs = rcode
-            rxmode = "DTCS"
-        elif _mem.rx_tone != 0xFFFF:
-            mem.ctone = _mem.rx_tone / 10.0
-            rxmode = "Tone"
-        else:
-            rxmode = ""
-
-        if txmode == "Tone" and not rxmode:
-            mem.tmode = "Tone"
-        elif txmode == rxmode and txmode == "Tone" and mem.rtone == mem.ctone:
-            mem.tmode = "TSQL"
-        elif txmode == rxmode and txmode == "DTCS" and mem.dtcs == mem.rx_dtcs:
-            mem.tmode = "DTCS"
-        elif rxmode or txmode:
-            mem.tmode = "Cross"
-            mem.cross_mode = "%s->%s" % (txmode, rxmode)
-
-        if mem.tmode == "DTCS":
-            mem.dtcs_polarity = "%s%s" % (tpol, rpol)
-
-        LOG.debug("Got TX %s (%i) RX %s (%i)" % (txmode, _mem.tx_tone,
-                                                 rxmode, _mem.rx_tone))
-
     def get_memory(self, number):
         _mem = self._memobj.memory[number - 1]
         _nam = self._memobj.chan_names[number - 1]
@@ -395,7 +356,7 @@ class IP620Radio(chirp_common.CloneModeRadio,
             mem.offset = abs(int(_mem.rx_freq) - int(_mem.tx_freq)) * 10
 
         mem.mode = _mem.w_n and "FM" or "NFM"
-        self._get_tone(_mem, mem)
+        self._tone_model.get_tone(_mem, mem)
         mem.power = POWER_LEVELS[_mem.power]
 
         mem.extra = RadioSettingGroup("Extra", "extra")
@@ -429,44 +390,6 @@ class IP620Radio(chirp_common.CloneModeRadio,
 
         return mem
 
-    def _set_tone(self, mem, _mem):
-        def _set_dcs(code, pol):
-            val = int("%i" % code, 8) + 0x2800
-            if pol == "R":
-                val += 0x8000
-            return val
-
-        rx_mode = tx_mode = None
-        rx_tone = tx_tone = 0xFFFF
-
-        if mem.tmode == "Tone":
-            tx_mode = "Tone"
-            rx_mode = None
-            tx_tone = int(mem.rtone * 10)
-        elif mem.tmode == "TSQL":
-            rx_mode = tx_mode = "Tone"
-            rx_tone = tx_tone = int(mem.ctone * 10)
-        elif mem.tmode == "DTCS":
-            tx_mode = rx_mode = "DTCS"
-            tx_tone = _set_dcs(mem.dtcs, mem.dtcs_polarity[0])
-            rx_tone = _set_dcs(mem.dtcs, mem.dtcs_polarity[1])
-        elif mem.tmode == "Cross":
-            tx_mode, rx_mode = mem.cross_mode.split("->")
-            if tx_mode == "DTCS":
-                tx_tone = _set_dcs(mem.dtcs, mem.dtcs_polarity[0])
-            elif tx_mode == "Tone":
-                tx_tone = int(mem.rtone * 10)
-            if rx_mode == "DTCS":
-                rx_tone = _set_dcs(mem.rx_dtcs, mem.dtcs_polarity[1])
-            elif rx_mode == "Tone":
-                rx_tone = int(mem.ctone * 10)
-
-        _mem.rx_tone = rx_tone
-        _mem.tx_tone = tx_tone
-
-        LOG.debug("Set TX %s (%i) RX %s (%i)" %
-                  (tx_mode, _mem.tx_tone, rx_mode, _mem.rx_tone))
-
     def set_memory(self, mem):
         _mem = self._memobj.memory[mem.number - 1]
         if mem.empty:
@@ -484,7 +407,7 @@ class IP620Radio(chirp_common.CloneModeRadio,
             _mem.tx_freq = mem.freq / 10
 
         _mem.w_n = mem.mode == "FM"
-        self._set_tone(mem, _mem)
+        self._tone_model.set_tone(mem, _mem)
         _mem.power = mem.power == POWER_LEVELS[1]
 
         for setting in ('lout', 'busy_loc', 'scan_add'):
