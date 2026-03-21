06baa58a268f2ab72e902b57af6090c07a3d40b3
Dan Smith <dsmith@danplanet.com>
2026-03-21 06:46:21 -0700

    hg_uv98: Move to common tone model
    I'm pretty sure this driver was actually broken for reverse DTCS,
    and this fixes it.

--- chirp/drivers/hg_uv98.py
+++ chirp/drivers/hg_uv98.py
@@ -25,6 +25,7 @@ import struct
 
 from chirp import chirp_common, directory, memmap, errors, util
 from chirp import bitwise
+from chirp import kenwood_tone
 from chirp.settings import RadioSettingGroup, RadioSetting
 from chirp.settings import RadioSettingValueBoolean, RadioSettingValueList
 from chirp.settings import RadioSettings
@@ -35,8 +36,8 @@ MEM_FORMAT = """
 struct {
   lbcd rx_freq[4];
   lbcd tx_freq[4];
-  ul16 rx_tone;
-  ul16 tx_tone;
+  ul16 rxtone;
+  ul16 txtone;
   u8 unknown1:6,
      wide:1,
      highpower:1;
@@ -306,6 +307,8 @@ class LanchonlhHG_UV98(chirp_common.CloneModeRadio, chirp_common.ExperimentalRad
     BAUD_RATE = 9600
 
     _upper = MAX_CHANNELS
+    _tone_model = kenwood_tone.KenwoodToneModel(
+        dcs_base=0x2800, pol_mask=0x8000, tone_init=0xFFFF, tone_flag=0x0000)
 
     @classmethod
     def get_prompts(cls):
@@ -370,45 +373,6 @@ class LanchonlhHG_UV98(chirp_common.CloneModeRadio, chirp_common.ExperimentalRad
     def get_raw_memory(self, number):
         return repr(self._memobj.memory[number - 1])
 
-    def _get_tone(self, _mem, mem):
-        def _get_dcs(val):
-            code = int("%03o" % (val & 0x07FF))
-            pol = (val & 0x8000) and "R" or "N"
-            return code, pol
-
-        if _mem.tx_tone != 0xFFFF and _mem.tx_tone > 0x2800:
-            tcode, tpol = _get_dcs(_mem.tx_tone)
-            mem.dtcs = tcode
-            txmode = "DTCS"
-        elif _mem.tx_tone != 0xFFFF:
-            mem.rtone = _mem.tx_tone / 10.0
-            txmode = "Tone"
-        else:
-            txmode = ""
-
-        if _mem.rx_tone != 0xFFFF and _mem.rx_tone > 0x2800:
-            rcode, rpol = _get_dcs(_mem.rx_tone)
-            mem.rx_dtcs = rcode
-            rxmode = "DTCS"
-        elif _mem.rx_tone != 0xFFFF:
-            mem.ctone = _mem.rx_tone / 10.0
-            rxmode = "Tone"
-        else:
-            rxmode = ""
-
-        if txmode == "Tone" and not rxmode:
-            mem.tmode = "Tone"
-        elif txmode == rxmode and txmode == "Tone" and mem.rtone == mem.ctone:
-            mem.tmode = "TSQL"
-        elif txmode == rxmode and txmode == "DTCS" and mem.dtcs == mem.rx_dtcs:
-            mem.tmode = "DTCS"
-        elif rxmode or txmode:
-            mem.tmode = "Cross"
-            mem.cross_mode = "%s->%s" % (txmode, rxmode)
-
-        if mem.tmode == "DTCS":
-            mem.dtcs_polarity = "%s%s" % (tpol, rpol)
-
     def get_memory(self, number):
 
         mem = chirp_common.Memory()
@@ -445,7 +409,7 @@ class LanchonlhHG_UV98(chirp_common.CloneModeRadio, chirp_common.ExperimentalRad
         else:
             mem.offset = 0
 
-        self._get_tone(_mem, mem)
+        self._tone_model.get_tone(_mem, mem)
         mem.power = POWER_LEVELS[_mem.highpower]
         mem.mode = MODES[_mem.wide]
         mem.skip = not _mem.scan and "S" or ""
@@ -457,44 +421,6 @@ class LanchonlhHG_UV98(chirp_common.CloneModeRadio, chirp_common.ExperimentalRad
         mem.extra.append(bcl)
 
         return mem
-
-    def _set_tone(self, mem, _mem):
-        def _set_dcs(code, pol):
-            val = int("%i" % code, 8) + 0x2800
-            if pol == "R":
-                val += 0xA000
-            return val
-
-        rx_mode = tx_mode = None
-        rx_tone = tx_tone = 0xFFFF
-
-        if mem.tmode == "Tone":
-            tx_mode = "Tone"
-            rx_mode = None
-            tx_tone = int(mem.rtone * 10)
-        elif mem.tmode == "TSQL":
-            rx_mode = tx_mode = "Tone"
-            rx_tone = tx_tone = int(mem.ctone * 10)
-        elif mem.tmode == "DTCS":
-            tx_mode = rx_mode = "DTCS"
-            tx_tone = _set_dcs(mem.dtcs, mem.dtcs_polarity[0])
-            rx_tone = _set_dcs(mem.dtcs, mem.dtcs_polarity[1])
-        elif mem.tmode == "Cross":
-            tx_mode, rx_mode = mem.cross_mode.split("->")
-            if tx_mode == "DTCS":
-                tx_tone = _set_dcs(mem.dtcs, mem.dtcs_polarity[0])
-            elif tx_mode == "Tone":
-                tx_tone = int(mem.rtone * 10)
-            if rx_mode == "DTCS":
-                rx_tone = _set_dcs(mem.rx_dtcs, mem.dtcs_polarity[1])
-            elif rx_mode == "Tone":
-                rx_tone = int(mem.ctone * 10)
-
-        _mem.rx_tone = rx_tone
-        _mem.tx_tone = tx_tone
-
-        LOG.debug("Set TX %s (%i) RX %s (%i)" %
-                  (tx_mode, _mem.tx_tone, rx_mode, _mem.rx_tone))
 
     def set_memory(self, mem):
         _mem = self._memobj.memory[mem.number - 1]
@@ -526,7 +452,7 @@ class LanchonlhHG_UV98(chirp_common.CloneModeRadio, chirp_common.ExperimentalRad
         else:
             _mem.tx_freq = mem.freq / 10
 
-        self._set_tone(mem, _mem)
+        self._tone_model.set_tone(mem, _mem)
 
         _mem.highpower = mem.power == POWER_LEVELS[1]
         _mem.wide = mem.mode == "NFM"
